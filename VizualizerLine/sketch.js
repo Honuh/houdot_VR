75e1a193a71b57fe110adb6861f145e3a7bf837d
Hugo Oudot <59360149+Honuh@users.noreply.github.com>
2022-12-24 18:41:22 +0100

    Update sketch.js

--- VizualizerLine/sketch.js
+++ VizualizerLine/sketch.js
@@ -1,16 +1,18 @@
 
 const agents = [];
 let sound;
-let lowmid;
+let highs;
+let lows;
+let test;
 let fft;
 
 function preload(){
-  sound = loadSound('2.mp3');
+  sound = loadSound('drums.mp3');
 }
 
 function setup() {
     createCanvas(1000, 1000);
-    for (var i = 0; i < 300; i++) {
+    for (var i = 0; i < 150; i++) {
       agents.push(new Agent (random(0,1000), random(0,1000), random(1,2)));
     }
     fft = new p5.FFT();
@@ -19,9 +21,21 @@ function setup() {
   
   function draw() {
     let spectrum = fft.analyze();
-    lowmid = map(fft.getEnergy('lowMid'),0,255,1,5);
+    
+    highs = map(fft.getEnergy(12000, 15000),0,255,1,2);
+    let high = highs**5;
+    lows = map(fft.getEnergy(160),0,255,1,2);
+    test = fft.getEnergy(map(mouseX,0,width,0,10000));
+    let low = lows**5;
     background(255);
+    console.log(test);
 
+    // for (let i = 0; i < spectrum.length; i++) {
+    //   var amp = spectrum[i];
+    //   var y = map(amp,0,255,500,0);
+    //   fill(0);
+    //   rect(i,y,1,height-y);
+    // }
     
     for (var i = 0; i < agents.length; i++) {
       const agent = agents[i];
@@ -32,7 +46,8 @@ function setup() {
   
         if(dist(agent.pos.x, agent.pos.y, other.pos.x, other.pos.y) <= 150) {
           stroke(0);
-          strokeWeight(map(dist(agent.pos.x, agent.pos.y, other.pos.x, other.pos.y), 0,150,2,0));
+          //strokeWeight(highs);
+          strokeWeight(map(dist(agent.pos.x, agent.pos.y, other.pos.x, other.pos.y), 0,150,2,0)*highs**4*2);
           line(agent.pos.x, agent.pos.y, other.pos.x, other.pos.y);
   
   
@@ -40,13 +55,16 @@ function setup() {
   
       }
       
-      // agents[i].vel.x *= lowmid;
-      agents[i].vel.y *= lowmid;
+      // agents[i].vel.x *= highs;
+      let oldX = agents[i].vel.x;
+      let oldY = agents[i].vel.y;
+      agents[i].vel.y *= high;
+      agents[i].vel.x *= low/2 ;
       
       // agents[i].draw();
       agents[i].update();
-      // agents[i].vel.x /= lowmid;
-      agents[i].vel.y /= lowmid;
+      agents[i].vel.x = oldX;
+      agents[i].vel.y = oldY;
       agents[i].bounce(width, height);
       // agents[i].wrap(width, height);
 
